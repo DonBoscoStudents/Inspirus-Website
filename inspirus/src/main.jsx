309fce9ee5350cdc804026e9df4fc947594433c7
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-08-23 23:40:27 +0530

    Default Component

--- inspirus/src/main.jsx
+++ inspirus/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './output.css'
+import Component from './components/ComponentTemplate'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <Component/>
+    <Component/>
+    <Component/>
+    <Component/>
   </React.StrictMode>
 )
 
