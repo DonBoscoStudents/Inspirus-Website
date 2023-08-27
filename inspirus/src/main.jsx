d9d338daf824be73f99d64b0549994a324be0174
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-08-27 21:52:53 +0530

    Prep For Merge 2

---
+++ inspirus/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './output.css'
+import Component from './components/ComponentTemplate'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Component/>
+    <Component/>
+    <Component/>
+    <Component/>
+  </React.StrictMode>
+)
+
+
+
+
+
