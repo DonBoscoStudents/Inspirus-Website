a8e67c59ea1789e6703e17a6ec95d3bd613a3c3b
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-08-30 19:08:21 +0530

    stuff

---
+++ inspirus/src/components/Form.jsx
@@ -0,0 +1,22 @@
+function Form() {
+    return  <>
+    <form target="blank" action="https://docs.google.com/forms/d/e/1FAIpQLSdG5u9egJEQEeNLweD206YOVZjkDV2eDsRNiyJI81B_HIW0qQ/formResponse">
+    <div className="page">
+    <div className="form_structure">
+    <div className="input_tab">
+    <p>Name</p>
+    <input name="entry.702823866" type="text" placeholder="elton costa" value="elton costa"></input>
+    </div>
+    <div className="input_tab">
+    <p>College</p>
+    <input name="entry.876583605" type="text" placeholder="Don bosco college of engineering" value="Don bosco college of engineering"></input>
+    </div>
+    <input type="submit"/>
+    </div>
+    </div>
+    </form>
+    </>
+  }
+  
+  export default Form;
+  
