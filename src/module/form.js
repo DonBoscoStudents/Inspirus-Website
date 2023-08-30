9a8187730c05a74b868306bea00ff027bad379ca
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2023-08-30 18:00:17 +0530

    implemented gforms api

--- src/module/form.js
+++ src/module/form.js
@@ -2,12 +2,21 @@ export default function Form()
 {
     return(
         <>
+        <form target="blank" action="https://docs.google.com/forms/d/e/1FAIpQLSdG5u9egJEQEeNLweD206YOVZjkDV2eDsRNiyJI81B_HIW0qQ/formResponse">
+        <div className="page">
         <div className="form_structure">
         <div className="input_tab">
-        <h3>name</h3>
-        <input type="text"></input>
+        <p>Name</p>
+        <input name="entry.702823866" type="text" placeholder="elton costa" value="elton costa"></input>
+        </div>
+        <div className="input_tab">
+        <p>College</p>
+        <input name="entry.876583605" type="text" placeholder="Don bosco college of engineering" value="Don bosco college of engineering"></input>
+        </div>
+        <input type="submit"/>
         </div>
         </div>
+        </form>
         </>
     )
 }
