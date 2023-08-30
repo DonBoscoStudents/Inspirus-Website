849cc60a7b3bf02ef229fc84dae4a71301d76b5b
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2023-08-31 01:48:29 +0530

    corrected css and added cordinator cards

---
+++ src/module/cordcard.js
@@ -0,0 +1,26 @@
+export default function Cord()
+{
+    return(
+        <>
+        <div className="cord_section">
+        <div className="coord_tab">
+            <img src="https://avatars.githubusercontent.com/u/89019551?v=4" alt=""></img>
+            <div className="temp">
+            <h3>Elton Costa</h3>
+            <p>Event Leader</p>
+            <a className="whatsapp" href="">Whatsapp</a>
+            </div>
+        </div>
+
+        <div className="coord_tab">
+            <img src="https://xsgames.co/randomusers/assets/avatars/male/74.jpg" alt=""></img>
+            <div className="temp">
+            <h3>Melrich Moraes</h3>
+            <p>Event Co-leader</p>
+            <a className="whatsapp" href="">Whatsapp</a>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
--- src/module/form.js
+++ src/module/form.js
@@ -1,38 +1,46 @@
+import Cord from "./cordcard"
+
 export default function Form()
 {
     return(
         <>
         <div className="page">
-        <form target="_self" action="https://docs.google.com/forms/d/e/1FAIpQLSdG5u9egJEQEeNLweD206YOVZjkDV2eDsRNiyJI81B_HIW0qQ/formResponse">
         <div className="form_structure">
+        <form action="https://docs.google.com/forms/d/e/1FAIpQLSdG5u9egJEQEeNLweD206YOVZjkDV2eDsRNiyJI81B_HIW0qQ/formResponse">
             <h1>INCARNATE</h1>
         <div className="input_tab">
         <p>Name</p>
         <input name="entry.702823866" type="text"></input>
         </div>
+
         <div className="input_tab">
-        <p>College/School/Diploma/Engineering</p>
-        <input name="entry.876583605" type="text"></input>
+        <p>email</p>
+        <input name="entry.702823866" type="text"></input>
         </div>
+
+        <div className="input_tab">
+        <p>College/School/Diploma</p>
+        <input name="entry.702823866" type="text"></input>
+        </div>
+
         <div className="input_tab">
         <p>Phone Number</p>
-        <input name="entry.876583605" type="text"></input>
+        <input name="entry.702823866" type="text"></input>
         </div>
+
         <div className="input_tab">
-        <p>Email</p>
-        <input name="entry.876583605" type="text"></input>
+        <p>Image Url</p>
+        <input name="entry.702823866" type="text"></input>
         </div>
+
         <div className="input_tab">
-        <p>Image URL</p>
-        <input name="entry.876583605" type="text"></input>
+        <p>Image Description</p>
+        <input name="entry.702823866" type="text"></input>
         </div>
-        <div className="input_tab">
-        <p>Image Caption</p>
-        <input name="entry.876583605" type="text"></input>
-        </div>
-        <input className="button" type="submit"/>
-        </div>
+        <input className="button" type="submit"></input>
         </form>
+        </div>
+        <Cord/>
         </div>
         </>
     )
