d1c762ff2c7a530e6e372b76b8fd29c40f576823
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-09-01 01:31:57 +0530

    Clean Up

--- inspirus/src/pages/Home/Home.jsx
+++ inspirus/src/pages/Home/Home.jsx
@@ -3,11 +3,7 @@ import MovingText from '../../components/movingText/app'
 import './style.module.css'
 import NavBar from '../../components/navBar/app'
 import PlaySound from '../../components/playSound/app'
-// function Hero() {
-//   return <div>
-//     <h1>Hello</h1>
-//   </div>;
-// }
+
 
 function Home() {
     return <div className='bg-primary text-textColor'>
@@ -15,6 +11,7 @@ function Home() {
       <PlaySound/>
       <Hero/>
       <MovingText/>
+
     </div>;
   }
   
