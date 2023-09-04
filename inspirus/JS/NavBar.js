8c9504f9e73ee219eece4fd4a774997ff5245b33
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-09-05 01:05:48 +0530

    Cards

--- inspirus/JS/NavBar.js
+++ inspirus/JS/NavBar.js
@@ -10,10 +10,51 @@ arrow_back
 <a>Home</a>
 <a>Team</a>
 </div>
-<button><span class="material-symbols-outlined">
+<button onclick="ChangeColorMode()"><span class="material-symbols-outlined">
 light_mode
 </span></button>
 `
 
 NavBar.classList.add('nav')
 document.body.appendChild(NavBar)
+
+
+window.onload=SetColorMode
+
+  let isDarkMode = true
+  function SetColorMode(){
+  
+
+    if(window.localStorage.getItem('color-scheme')=='dark'){
+      document.body.classList.add('dark')
+      document.body.classList.remove('light')
+      isDarkMode=true
+
+    }else if(window.localStorage.getItem('color-scheme')=='light'){
+      document.body.classList.add('light')
+      document.body.classList.remove('dark')
+      isDarkMode=false
+
+    }else{
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+            document.body.classList.add('dark')
+            isDarkMode=true
+          }else{
+            document.body.classList.add('light')
+            isDarkMode=false
+          }
+    }
+  }
+  function ChangeColorMode(){
+    isDarkMode=!isDarkMode
+    document.body.classList.toggle('dark')
+    document.body.classList.toggle('light')
+    if(isDarkMode){
+      window.localStorage.setItem('color-scheme','dark')
+        console.log(window.localStorage)
+    }else{
+      window.localStorage.setItem('color-scheme','light')
+      console.log(window.localStorage)
+
+    }
+  }
