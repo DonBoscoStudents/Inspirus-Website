b78baddaffab71a77e119cbe38c90868b0a710d2
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-09-05 00:34:16 +0530

    Main Page

--- inspirus/JS/NavBar.js
+++ inspirus/JS/NavBar.js
@@ -3,7 +3,16 @@
 const NavBar= document.createElement('div')
 
 NavBar.innerHTML=`
-Hello
+<button><span class="material-symbols-outlined">
+arrow_back
+</span></button>
+<div>
+<a>Home</a>
+<a>Team</a>
+</div>
+<button><span class="material-symbols-outlined">
+light_mode
+</span></button>
 `
 
 NavBar.classList.add('nav')
