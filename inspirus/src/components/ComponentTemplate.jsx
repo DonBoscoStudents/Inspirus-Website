77fb890ca34241d2bc9f7ef3c2da747f11452012
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-09-05 23:40:36 +0530

    Revert "Build2"

---
+++ inspirus/src/components/ComponentTemplate.jsx
@@ -0,0 +1,7 @@
+function Component() {
+  return <div>
+    <h1>This Is A Component</h1>
+  </div>;
+}
+
+export default Component;
--- inspirus/tailwind.config.js
+++ inspirus/tailwind.config.js
@@ -2,35 +2,15 @@
 export default {
   content: [
   "./index.html",
-  "./Pages/**/*.{js,ts,jsx,tsx}",
+  "./src/**/*.{js,ts,jsx,tsx}",
   "./src/pages/LoadingPage/App.jsx"
 ],
   theme: {
     fontFamily:{
-      'minecraft':'Minecraft',
-      'rubik':'Rubik',
-      'NotoSans':'Rubik',
-      'NotoSerif':'Rubik',
-      'IBMPlexSerif':'IBMPlexSerif',
-      'ElegantoSans':'ElegantoSans',
-      'Poppins':'Poppins',
+      'minecraft':'Minecraft'
     },
-    extend: {
-      colors: {
-        "primary": "var(--primary)",
-        "primary-2": "var(--primary-2)",
-        "textColor": "var(--textColor)",
-        "c1": "var(--c1)",
-        "c2": "var(--c2)",
-        "c3": "var(--c3)",
-        "c4": "var(--c4)",
-        "c5": "var(--c5)",
-        "c6": "var(--c6)"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
-
-
 
