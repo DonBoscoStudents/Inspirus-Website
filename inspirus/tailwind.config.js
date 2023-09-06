147af032cf532bcd912c0f3bbd1ef6b8b36d34bb
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-09-06 17:54:55 +0530

    team

--- inspirus/tailwind.config.js
+++ inspirus/tailwind.config.js
@@ -4,7 +4,8 @@ export default {
   "./index.html",
   "./Pages/**/*.{js,ts,jsx,tsx,html}",
   "./src/pages/LoadingPage/App.jsx",
-  "Pages/Event/Incarnate/index.html"
+  "Pages/Event/Incarnate/index.html",
+  "Pages/Team/index.html"
 ],
   theme: {
     fontFamily:{
@@ -21,6 +22,7 @@ export default {
         "primary": "var(--primary)",
         "primary-2": "var(--primary-2)",
         "textColor": "var(--textColor)",
+        "textColorHover": "var(--textColorHover)",
         "c1": "var(--c1)",
         "c2": "var(--c2)",
         "c3": "var(--c3)",
