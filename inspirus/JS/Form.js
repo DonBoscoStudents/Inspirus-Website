9c251e505e2b068ab9d9c26733085e79770fe7fd
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-09-06 16:10:08 +0530

    Did Some Changes

---
+++ inspirus/JS/Form.js
@@ -0,0 +1,5 @@
+// https://docs.google.com/forms/d/e/1FAIpQLScx5Nh1ivF_cEcJU-hom20ZU24CfjWOFafziE3LNrYN4MvSow/viewform?usp=pp_url&entry.1501360210=fghfh&entry.831290339=Yes&entry.1787289822=Yes
+
+function RegisterForm(link,preFilledLink){
+    window.location.href=link
+}
