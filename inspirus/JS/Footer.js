74b350237fe822ef521bceac473d60d34fab79cf
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-09-07 02:20:51 +0530

    Added Map to Footer

--- inspirus/JS/Footer.js
+++ inspirus/JS/Footer.js
@@ -1,7 +1,7 @@
 
 const Footer= document.createElement('div')
 
-Footer.innerHTML=``
+Footer.innerHTML=`<div></div><div><iframe title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3848.5562037216996!2d73.96689591435725!3d15.291979163859711!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbfb1631736de9d%3A0x980720b4516a7a5!2sDon%20Bosco%20College%20Of%20Engineering!5e0!3m2!1sen!2sin!4v1670427168557!5m2!1sen!2sin" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>`
 
 
 Footer.classList.add('footer')
